f6028705b3414f62e6085321b32545fc098d1700
Hatzz <git@hatzz.io>
2021-06-12 22:45:34 +0200

    Add mock navigation items

--- components/nav/Bar.vue
+++ components/nav/Bar.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="flex justify-center">
     <NavItem to="/#new-ducks">
-      Newly minted ducks
+      Newly Minted Ducks
+    </NavItem>
+    <NavItem to="/#what-the-duck">
+      What the duck?
+    </NavItem>
+    <NavItem to="/#roadmap">
+      Roadmap
     </NavItem>
     <img src="~/assets/icons/duck.svg" alt="Duck icon">
+    <NavItem to="/#tokenomics">
+      Tokenomics
+    </NavItem>
+    <NavItem to="/#pond-papers">
+      Pond papers
+    </NavItem>
+    <NavItem to="/#pond-papers">
+      Pond papers
+    </NavItem>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
