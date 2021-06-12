97055d7f67abbc5863e2c34aef6ca162c6532373
Hatzz <git@hatzz.io>
2021-06-12 22:40:32 +0200

    Create social icon component and socials component

---
+++ components/Social.vue
@@ -0,0 +1,18 @@
+<template>
+  <img :src="require(`~/assets/icons/${icon}.svg`)" :alt="name" class="w-12 md:w-16">
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
---
+++ components/Socials.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-row justify-evenly md:flex-col">
+    <Social icon="telegram" name="Telegram" />
+    <Social icon="discord" name="Discord" />
+    <Social icon="medium" name="Medium" />
+    <Social icon="twitter" name="Twitter" />
+    <Social icon="linkedin" name="Linkedin" />
+  </div>
+</template>
