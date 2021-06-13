8163a0b80f05a3eaa8680f57138a06089c5b76a6
Hatzz <git@hatzz.io>
2021-06-13 21:26:28 +0200

    Add roadmap section and roadmap duck component

---
+++ components/Roadmap.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="flex flex-col w-full justify-evenly lg:flex-row">
+    <RoadmapDuck img-src="running-duck-1.png" img-alt="Duck walking">
+      <template slot="title">
+        Q1
+      </template>
+      <template slot="items">
+        <P>Token mint</P>
+      </template>
+    </RoadmapDuck>
+    <RoadmapDuck img-src="running-duck-1.png" img-alt="Duck walking" class="mt-8 lg:mt-0">
+      <template slot="title">
+        Q2
+      </template>
+      <template slot="items">
+        <P>NFDs</P>
+        <P>NFD Platform</P>
+        <P>Governance</P>
+        <P>Duck Sniper</P>
+      </template>
+    </RoadmapDuck>
+    <RoadmapDuck img-src="running-duck-2.png" img-alt="Duck starting to sprint" class="mt-8 lg:mt-0">
+      <template slot="title">
+        Q3
+      </template>
+      <template slot="items">
+        <P>Hatchling Charts</P>
+        <P>Merch</P>
+        <P>ZilLotto</P>
+      </template>
+    </RoadmapDuck>
+    <RoadmapDuck img-src="running-duck-3.png" img-alt="Duck starting to sprint" class="mt-8 lg:mt-0">
+      <template slot="title">
+        Q4
+      </template>
+      <template slot="items">
+        <P>Deep Duck Render</P>
+        <P>Limit Orders</P>
+      </template>
+    </RoadmapDuck>
+  </section>
+</template>
---
+++ components/RoadmapDuck.vue
@@ -0,0 +1,30 @@
+<template>
+  <article class="flex flex-col items-center">
+    <H2 class="font-semibold">
+      <slot name="title" />
+    </H2>
+    <img
+      :src="require(`~/assets/images/${imgSrc}`)"
+      class="w-48"
+      :alt="imgAlt"
+    >
+    <div class="flex flex-col items-center">
+      <slot name="items" />
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    imgSrc: {
+      type: String,
+      required: true
+    },
+    imgAlt: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
