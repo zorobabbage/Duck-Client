8d33241738921437153144712364f18f0e5458ac
Hatzz <git@hatzz.io>
2021-06-13 21:26:47 +0200

    Add section explaining the duck token

---
+++ components/WhatTheDuck.vue
@@ -0,0 +1,26 @@
+<template>
+  <section
+    class="flex flex-col justify-evenly m-4 lg:flex-row"
+  >
+    <img
+      src="~/assets/images/question-duck.png"
+      alt="Duck with a question mark"
+      class="lg:w-1/2"
+    >
+    <article class="flex flex-col items-center justify-center lg:w-1/2">
+      <H2 class="self-start">
+        What the duck?
+      </H2>
+      <P class="mt-2">
+        Duck is a fledgling token on the Zilliqa blockchain, with lofty
+        ambitions of creating a suite of tools to attract more users to the
+        ecosytem.
+      </P>
+      <P class="mt-4">
+        As a small team of 4 (soon to be 5), our initial goal is to release
+        in-house designed NFTs. This will be followed by the launch of an NFT
+        platform which artists can use to mint and sell their products.
+      </P>
+    </article>
+  </section>
+</template>
