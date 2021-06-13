bb79eed69d8f0fa8df036273ca007c4271a20be4
Hatzz <git@hatzz.io>
2021-06-13 21:25:42 +0200

    Add the section for showing five newly minted ducks

---
+++ components/NewlyMinted.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="flex flex-col items-center">
+    <H2 class="text-center max-w-lg xl:max-w-2xl 2xl:max-w-4xl">
+      Yes we are incredibly retarded, here are some newly minted ducks:
+    </H2>
+    <div class="flex flex-col items-center lg:flex-row lg:justify-center">
+      <img
+        v-for="n in 5"
+        :key="n"
+        src="~/assets/images/unkown-duck.png"
+        alt="Unkown duck"
+        class="lg:w-48 xl:w-64 2xl:w-80"
+      >
+    </div>
+    <Link href="#" class="mt-8">
+      Show me some more duckies please
+    </Link>
+  </section>
+</template>
