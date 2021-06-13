6624ab6c736d23a823ea38322a19f17ea895cf6e
Hatzz <git@hatzz.io>
2021-06-13 17:16:08 +0200

    Create page hero

---
+++ components/Hero.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="flex flex-col items-center lg:flex-row lg:justify-center">
+    <div class="m-4 flex flex-col lg:w-1/2">
+      <H1 class="">
+        <span class="text-duck-gradient">Duck</span>, the community project
+        swimming on the <span class="text-zilliqa">Zilliqa</span> blockchain
+      </H1>
+      <P class="mt-4">
+        Building a suite of tools for the Zilliqa ecosystem including NFD
+        minting and deployments, presale sniping and charting tools
+      </P>
+      <button class="text-l p-4 font-bold bg-duck-yellow rounded-md mt-4 w-36 lg:self-end xl:text-xl xl:w-48 2xl:text-2xl 2xl:w-52">Launch dapp</button>
+    </div>
+    <img src="~/assets/images/computer-duck.png" alt="Duck with glasses sitting by a computer" class="lg:w-1/2">
+  </section>
+</template>
