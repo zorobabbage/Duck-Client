5765207d794052af76fb7c5a0dcfa3b7f8d5d8cc
Hatzz <git@hatzz.io>
2021-06-13 21:27:01 +0200

    Add tokenomics sections

---
+++ components/Tokenomics.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="flex flex-col">
+    <article>
+      <H2>Ser pls how many duckos?</H2>
+      <P>
+        $DUCK has a total supply of 420.69, with a circulating supply of (pull
+        from Zilstream).
+      </P>
+      <P class="mt-8">
+        The community wallet holds a majority of the non-circulating supply. The
+        purpose of the community wallet is to actively engage and compensate
+        members of the Zilliqa ecosystem through airdrops, LP rewards and
+        giveaways.
+      </P>
+    </article>
+  </section>
+</template>
