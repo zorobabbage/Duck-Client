f1479520443b679bcc7dbd66cde1e70de908c8dd
Hatzz <git@hatzz.io>
2021-06-14 10:25:46 +0200

    Create tokenomics section

---
+++ components/TokenomicsDuck.vue
@@ -0,0 +1,23 @@
+<template>
+  <article class="flex flex-col items-center">
+    <img class="w-48" :src="require(`~/assets/images/${img}`)" :alt="alt">
+    <P class="text-center">
+      <slot />
+    </P>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    img: {
+      type: String,
+      required: true
+    },
+    alt: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
--- components/sections/Tokenomics.vue
+++ components/sections/Tokenomics.vue
@@ -1,6 +1,6 @@
 <template>
-  <section class="flex flex-col">
-    <article>
+  <section class="flex flex-col lg:flex-row">
+    <article class="flex flex-col lg:w-1/2">
       <H2>Ser pls how many duckos?</H2>
       <P>
         $DUCK has a total supply of 420.69, with a circulating supply of (pull
@@ -13,5 +13,19 @@
         giveaways.
       </P>
     </article>
+    <div class="flex flex-col items-center lg:flex-row lg:w-1/2 lg:justify-evenly">
+      <TokenomicsDuck img="duck-egg-feet.png" alt="Duck cracking its legs out of the egg">
+        Initial supply<br>
+        420.69
+      </TokenomicsDuck>
+      <TokenomicsDuck class="mt-16 lg:mt-0" img="duck-egg-crack.png" alt="Duck cracking the top of the egg">
+        Max supply<br>
+        391
+      </TokenomicsDuck>
+      <TokenomicsDuck class="mt-16 lg:mt-0" img="duck-egg-head.png" alt="Duck cracking its head out of the egg">
+        Circulating supply<br>
+        305
+      </TokenomicsDuck>
+    </div>
   </section>
 </template>
