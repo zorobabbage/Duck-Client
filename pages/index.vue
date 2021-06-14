d4b4acc27607e1fa3b092839b76739b9879fd588
Hatzz <git@hatzz.io>
2021-06-14 10:26:40 +0200

    Add sections to index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div />
+  <div class="flex flex-col items-center max-w-screen-md lg:max-w-none mx-auto">
+    {{ $breakpoint }}
+    <Hero id="#home" class="max-w-[2400px]" />
+    <NewlyMinted id="#new-ducks" class="mt-12 lg:mt-24" />
+    <WhatTheDuck id="#what-the-duck" class="mt-4 max-w-screen-2xl lg:mt-24" />
+    <Roadmap id="#roadmap" class="max-w-screen-2xl mt-24" />
+    <Tokenomics id="#tokenomics" class="m-4 max-w-screen-2xl mt-24" />
+  </div>
 </template>
 
 <script>
