8c64b322758a222865fca77d105fd5b6e93dd55a
Hatzz <git@hatzz.io>
2021-06-14 10:43:18 +0200

    Create pond paper section

---
+++ components/sections/PondPapers.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="flex flex-col items-center lg:flex-row lg:justify-evenly">
+    <div class="flex flex-col justify-center items-center lg:w-1/3">
+      <Link href="#">
+        <H2>NFDs</H2>
+      </Link>
+    </div>
+    <img
+      src="~/assets/images/duck-reading.png"
+      alt="Duck holding and reading a piece of paper"
+      class="lg:w-1/3"
+    >
+    <div class="flex flex-col justify-center items-center lg:w-1/3">
+      <H2 class="text-blue-500">
+        Whitepaper soon
+      </H2>
+    </div>
+  </section>
+</template>
