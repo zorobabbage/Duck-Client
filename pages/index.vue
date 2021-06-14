499ce3a4848d8e0f3967dca0038ca887dd470456
Hatzz <git@hatzz.io>
2021-06-14 17:12:03 +0200

    Fix ids for hyperlinks

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="flex flex-col items-center max-w-screen-md lg:max-w-none mx-auto">
-    {{ $breakpoint }}
-    <Hero id="#home" class="max-w-[2400px]" />
-    <NewlyMinted id="#new-ducks" class="mt-12 lg:mt-24" />
-    <WhatTheDuck id="#what-the-duck" class="mt-4 w-full max-w-screen-2xl lg:mt-24" />
-    <Roadmap id="#roadmap" class="w-full max-w-screen-2xl mt-24" />
-    <Tokenomics id="#tokenomics" class="w-full max-w-screen-2xl mt-24" />
-    <PondPapers id="#pond-papers" class="w-full max-w-screen-2xl mt-24" />
+    <Hero id="home" class="max-w-[2400px]" />
+    <NewlyMinted id="new-ducks" class="mt-12 lg:mt-24" />
+    <WhatTheDuck
+      id="what-the-duck"
+      class="mt-4 w-full max-w-screen-2xl lg:mt-24"
+    />
+    <Roadmap id="roadmap" class="w-full max-w-screen-2xl mt-24" />
+    <Tokenomics id="tokenomics" class="w-full max-w-screen-2xl mt-24" />
+    <PondPapers id="pond-papers" class="w-full max-w-screen-2xl mt-24" />
+    <Footer />
   </div>
 </template>
 
