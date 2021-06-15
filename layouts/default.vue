f7364ed3d659a9d2d5c01d5754071df2e5ff0ae9
Hatzz <git@hatzz.io>
2021-06-15 16:45:19 +0200

    Add observer for changing active url on scroll

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
-    <BurgerMenu v-if="breakpoint.is === 'sm' || breakpoint.is === 'md'" />
-    <NavBar v-else />
+    <BurgerMenu :active-link="activeLink" v-if="breakpoint.is === 'sm' || breakpoint.is === 'md'" />
+    <NavBar :active-link="activeLink" v-else />
     <Nuxt />
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      sectionObserver: undefined,
+      activeLink: ''
+    }
+  },
   head: {
     bodyAttrs: {
       class: 'bg-gray-50'
@@ -19,6 +25,41 @@ export default {
         return this.$breakpoint
       }
       return { is: '' }
+    }
+  },
+  mounted () {
+    this.observeSections()
+  },
+  methods: {
+    observeSections () {
+      try {
+        this.sectionObserver.disconnect()
+      } catch (error) {}
+
+      const options = {
+        rootMargin: '50px 50px',
+        threshold: 1
+      }
+      this.sectionObserver = new IntersectionObserver(
+        this.sectionObserverHandler,
+        options
+      )
+
+      // Observe each section
+      const sections = document.querySelectorAll('section')
+      sections.forEach((section) => {
+        this.sectionObserver.observe(section)
+      })
+    },
+    sectionObserverHandler (entries) {
+      for (const entry of entries) {
+        if (entry.isIntersecting) {
+          const sectionId = entry.target.id
+          // Push sectionId to router here
+          history.replaceState(null, null, `#${sectionId}`)
+          this.activeLink = `#${sectionId}`
+        }
+      }
     }
   }
 }
