7fcc0f036385ef1e20e916fa0d8bfb800c61545c
Hatzz <git@hatzz.io>
2021-06-16 18:49:08 +0200

    Add financial disclaimer

--- components/sections/Footer.vue
+++ components/sections/Footer.vue
@@ -1,5 +1,16 @@
 <template>
-  <footer class="flex flex-col p-16">
-    <P>Go duck yourself this is not financial advice</P>
+  <footer class="flex flex-col items-center p-16">
+    <p class="max-w-screen-lg text-sm">
+      We do not accept any liability for any loss or damage which is incurred
+      from you acting or not acting as a result of reading any of our
+      publications. Our publications do not offer investment advice and nothing
+      in them should be construed as investment advice. Our publications provide
+      information and education for investors who can make their investment
+      decisions without advice. The information contained in our publications is
+      not, and should not be read as, an offer or recommendation to buy or sell
+      or a solicitation of an offer or recommendation to buy or sell any
+      securities.
+    </p>
+    <P>DUCK© 2021</P>
   </footer>
 </template>
