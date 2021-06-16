631482bd369d88af2ba84b8e7c74536880ec6aef
Hatzz <git@hatzz.io>
2021-06-16 16:24:09 +0200

    Move call to action in to seperate component

--- components/sections/Hero.vue
+++ components/sections/Hero.vue
@@ -18,11 +18,7 @@
         </Link>
       </P>
 
-      <button
-        class="text-l p-4 font-bold bg-duck-yellow rounded-md mt-4 w-36 lg:self-end xl:text-xl xl:w-48 2xl:text-2xl 2xl:w-52"
-      >
-        <a href="https://testingtheducks.com">Launch dapp</a>
-      </button>
+      <CallToAction />
     </div>
     <img
       src="~/assets/images/computer-duck.png"
