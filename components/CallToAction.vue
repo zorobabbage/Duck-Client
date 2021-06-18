6b58b0446fb7e20766b000de61a8fa7733f81383
Hatzz <git@hatzz.io>
2021-06-18 16:42:24 +0200

    Change url to production url

--- components/CallToAction.vue
+++ components/CallToAction.vue
@@ -2,6 +2,6 @@
   <button
     class="text-l p-4 font-bold bg-duck-yellow rounded-md mt-4 w-36 lg:self-end xl:text-xl xl:w-48 2xl:text-2xl 2xl:w-52"
   >
-    <a href="https://testingtheducks.com">Launch dapp</a>
+    <a href="https://app.duck.community/duckstore">Launch dapp</a>
   </button>
 </template>
