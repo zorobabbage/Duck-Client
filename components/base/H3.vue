e14749782de58c92a2131eca48577fd2c12e27c5
Hatzz <git@hatzz.io>
2021-06-21 22:05:01 +0200

    Actually fix the newly minted ducks this time

---
+++ components/base/H3.vue
@@ -0,0 +1,5 @@
+<template>
+  <h2 class="text-2xl xl:text-3xl 2xl:text-4xl font-medium">
+    <slot />
+  </h2>
+</template>
--- components/sections/NewlyMinted.vue
+++ components/sections/NewlyMinted.vue
@@ -7,13 +7,20 @@
       v-if="show"
       class="flex flex-col items-center lg:flex-row lg:justify-center"
     >
-      <img
+      <div
         v-for="duckUri in latestFiveDuckUris"
         :key="duckUri.id"
-        :src="duckUri.uri"
-        alt="Unkown duck"
-        class="lg:w-48 xl:w-64 2xl:w-80"
+        class="flex-col items-center justify-center"
       >
+        <img
+          :src="duckUri.uri"
+          alt="Unkown duck"
+          class="lg:w-48 xl:w-64 2xl:w-80"
+        >
+        <H3 class="text-center mt-4">
+          #{{ duckUri.id }}
+        </H3>
+      </div>
     </div>
     <Link
       v-if="show && latestFiveDuckUris.length > 0"
@@ -30,7 +37,11 @@
       <H2 class="text-center max-w-lg xl:max-w-2xl 2xl:max-w-4xl">
         {{ message }}
       </H2>
-      <button class="p-4 bg-zilliqa rounded-2xl mt-4" @click="connect">
+      <button
+        v-if="showConnectButton"
+        class="p-4 bg-zilliqa rounded-2xl mt-4"
+        @click="connect"
+      >
         Connect
       </button>
     </div>
@@ -42,7 +53,9 @@ export default {
   data () {
     return {
       show: false,
-      message: 'Please connect your wallet to show newly minted ducks!'
+      message: 'Please connect your wallet to show newly minted ducks!',
+      connected: false,
+      showConnectButton: false
     }
   },
   computed: {
@@ -51,42 +64,52 @@ export default {
       if (duckUris.length <= 5) {
         return duckUris
       }
-      return duckUris.slice(duckUris.length - 5, duckUris.length)
+      return duckUris.slice(duckUris.length - 5, duckUris.length).reverse()
     }
   },
   mounted () {
-    if (window.zilPay === undefined) {
-      this.show = false
-      this.message =
-        'Please install the zilpay extension to show newly minted ducks'
-      return
-    }
-
-    const networkStream = window.zilPay.wallet.observableNetwork()
-    networkStream.subscribe((net) => {
-      if (net !== process.env.zilliqaNetwork) {
-        this.show = false
-        this.message = `Please choose ${process.env.zilliqaNetwork} in your wallet`
-        return
-      }
-      this.show = true
-    })
+    this.connect()
   },
   methods: {
-    connect () {
+    async connect () {
+      if (!process.browser) {
+        return
+      }
       if (window.zilPay === undefined) {
         this.show = false
         this.message =
           'Please install the zilpay extension to show newly minted ducks'
+        this.showConnectButton = false
         return
       }
+
+      this.showConnectButton = true
+
+      this.connected = await window.zilPay.wallet.connect()
+      if (!this.connected) {
+        this.show = false
+        this.message = 'Please connect your wallet to show newly minted ducks!'
+        return
+      }
+
       if (window.zilPay.wallet.net !== process.env.zilliqaNetwork) {
         this.show = false
         this.message = `Please choose ${process.env.zilliqaNetwork} in your wallet`
-      } else {
-        this.$store.dispatch('ducks/fetchDuckUris')
-        this.show = true
+        this.showConnectButton = false
+        return
       }
+
+      const networkStream = window.zilPay.wallet.observableNetwork()
+      networkStream.subscribe((net) => {
+        if (net !== process.env.zilliqaNetwork) {
+          this.show = false
+          this.message = `Please choose ${process.env.zilliqaNetwork} in your wallet`
+          this.showConnectButton = false
+        }
+      })
+
+      this.$store.dispatch('ducks/fetchDuckUris')
+      this.show = true
     }
   }
 }
