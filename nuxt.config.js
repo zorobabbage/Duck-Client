ba5a86d51dd4b8c07b4c0481191f0c0f2a1dde58
Babbage <qingpo.gao@gmail.com>
2023-05-28 22:57:10 +0800

    update config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
-export function getApiUrl () {
-  return 'https://api.duck.community'
+export function getApiUrl() {
+  // return 'https://api.duck.community'
+  return "https://duck-api.envizion.world";
 }
-
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   // target: 'static',
@@ -15,75 +15,73 @@ export default {
     migrator_contract: process.env.migrator_contract,
     rewards_contract: process.env.rewards_contract,
     voucher_contract: process.env.voucher_contract,
-    ownership_contract: process.env.ownership_contract
+    ownership_contract: process.env.ownership_contract,
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Duck - Non Fungible Ducks',
+    title: "Duck - Non Fungible Ducks",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: 'description',
-        name: 'description',
-        content: 'Home of the Non Fungible Ducks as well as all projects by the Duck team.'
+        hid: "description",
+        name: "description",
+        content:
+          "Home of the Non Fungible Ducks as well as all projects by the Duck team.",
       },
       {
-        hid: 'og:image',
-        property: 'og:image',
-        content: '/meta-preview.png'
-      }
+        hid: "og:image",
+        property: "og:image",
+        content: "/meta-preview.png",
+      },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700;800&display=swap' }
-    ]
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700;800&display=swap",
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    '~/plugins/breakpoint.client.js',
-    '~/plugins/changeToast.js'],
+  plugins: ["~/plugins/breakpoint.client.js", "~/plugins/changeToast.js"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: {
-    dirs: ['~/components', '~/components/sections', '~/components/base']
+    dirs: ["~/components", "~/components/sections", "~/components/base"],
   },
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
     // '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss'
+    "@nuxtjs/tailwindcss",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    '@nuxtjs/axios',
-    '@nuxtjs/toast',
-    '@nuxt/content'
-  ],
+  modules: ["@nuxtjs/axios", "@nuxtjs/toast", "@nuxt/content"],
 
   axios: {
-    baseURL: getApiUrl()
+    baseURL: getApiUrl(),
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    extend (config) {
+    extend(config) {
       config.node = {
-        fs: 'empty'
-      }
-    }
+        fs: "empty",
+      };
+    },
   },
   server: {
-    host: '0.0.0.0'
-  }
-}
+    host: "0.0.0.0",
+  },
+};
